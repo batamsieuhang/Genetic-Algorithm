31f9416c596bb1f48ccc66d7d834743fcda8e5dc
TuDo16 <20021460@vnu.edu.vn>
2023-02-28 01:02:21 +0700

    add chelsea team squad

---
+++ GA_Chelsea.py
@@ -0,0 +1,191 @@
+
+
+# player position
+# GK-GoalKeeper
+# DF-Defender
+# CM-Central Midfielder
+# AT-Attacker
+player_chelsea = {
+    "GK": {
+        "Edouard Mendy": {
+            "position": "GK",
+            "rating": 6.5,
+            "value": 25,
+            "flag": 0
+        },
+        "Kepa Arrizabalaga": {
+            "position": "GK",
+            "rating": 7.02,
+            "value": 15,
+            "flag": 0
+        },
+        "Marcus Bettinelli": {
+            "position": "GK",
+            "rating": 5.0,
+            "value": 2,
+            "flag": 0
+        },
+    },
+    "DF": {
+        "Wesley Fofana": {
+            "position": "DF",
+            "rating": 7.48,
+            "value": 65,
+            "flag": 0
+        },
+        "Kalidou Koulibaly": {
+            "position": "DF",
+            "rating": 6.82,
+            "value": 35,
+            "flag": 0
+        }, "Trevoh Chalobah": {
+            "position": "DF",
+            "rating": 6.94,
+            "value": 22,
+            "flag": 0
+        }, "Thiago Silva": {
+            "position": "DF",
+            "rating": 6.93,
+            "value": 2,
+            "flag": 0
+        },
+        "Marc Cucurella": {
+            "position": "DF",
+            "rating": 6.54,
+            "value": 55,
+            "flag": 0
+        },
+        "Ben Chilwell": {
+            "position": "DF",
+            "rating": 6.97,
+            "value": 38,
+            "flag": 0
+        },
+        "Reece James": {
+            "position": "DF",
+            "rating": 7.19,
+            "value": 70,
+            "flag": 0
+        },
+        "César Azpilicueta": {
+            "position": "DF",
+            "rating": 6.29,
+            "value": 8,
+            "flag": 0
+        },
+    },
+    "CM": {
+        "Denis Zakaria": {
+            "position": "CM",
+            "rating": 7.51,
+            "value": 20,
+            "flag": 0
+        },
+        "N\'Golo Kanté": {
+            "position": "CM",
+            "rating": 7.2,
+            "value": 30,
+            "flag": 0
+        },
+        "Denis Zakaria": {
+            "position": "CM",
+            "rating": 7.51,
+            "value": 20,
+            "flag": 0
+        },
+        "Enzo Fernández": {
+            "position": "CM",
+            "rating": 6.81,
+            "value": 55,
+            "flag": 0
+        },
+        "Mateo Kovacic": {
+            "position": "CM",
+            "rating": 6.85,
+            "value": 40,
+            "flag": 0
+        },
+        "Conor Gallagher": {
+            "position": "CM",
+            "rating": 6.57,
+            "value": 32,
+            "flag": 0
+        },
+        "Ruben Loftus-Cheek": {
+            "position": "CM",
+            "rating": 6.6,
+            "value": 25,
+            "flag": 0
+        },
+        "Mason Mount": {
+            "position": "CM",
+            "rating":  6.89,
+            "value": 75,
+            "flag": 0
+        },
+        "Kai Havertz": {
+            "position": "CM",
+            "rating": 6.75,
+            "value": 70,
+            "flag": 0
+        },
+    },
+    "AT": {
+        "Raheem Sterling": {
+            "position": "AT",
+            "rating": 6.66,
+            "value": 70,
+            "flag": 0
+        },
+        "Mykhaylo Mudryk": {
+            "position": "AT",
+            "rating": 6.15,
+            "value": 40,
+            "flag": 0
+        },
+        "Christian Pulisic": {
+            "position": "AT",
+            "rating": 6.31,
+            "value": 24,
+            "flag": 0
+        },
+        "Hakim Ziyech": {
+            "position": "AT",
+            "rating": 6.47,
+            "value": 20,
+            "flag": 0
+        },
+        "Noni Madueke": {
+            "position": "AT",
+            "rating": 6.77,
+            "value": 15,
+            "flag": 0
+        },
+        "João Félix": {
+            "position": "AT",
+            "rating": 6.55,
+            "value": 50,
+            "flag": 0
+        },
+        "Armando Broja": {
+            "position": "AT",
+            "rating": 6.29,
+            "value": 30,
+            "flag": 0
+        },
+        "Pierre-Emerick Aubameyang": {
+            "position": "AT",
+            "rating": 6.15,
+            "value": 12,
+            "flag": 0
+        },
+        "David Datro Fofana": {
+            "position": "AT",
+            "rating": 6.61,
+            "value": 7,
+            "flag": 0
+        },
+    },
+
+
+}
